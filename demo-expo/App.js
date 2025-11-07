d948edb4b589177c454b144e3c0222acdcf82561
fMmUd <mongiatfranco@udesa.edu.ar>
2025-11-07 00:49:04 -0300

    cree el componente CreatePost y agrege su tabscreen

--- demo-expo/App.js
+++ demo-expo/App.js
@@ -1,12 +1,18 @@
+import React from "react";
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+
 import Login from './src/screens/Login';
 import Register from './src/screens/Register';
 import HomeMenu from './src/components/HomeMenu';
 
+
 const Stack = createNativeStackNavigator()
 
+
 export default function App() {
+
   return (
     <NavigationContainer>
       <Stack.Navigator>
---
+++ demo-expo/src/components/CreatePost.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+import { View, Text, StyleSheet, Pressable } from "react-native";
+import { TextInput } from "react-native-web";
+import { db, auth } from "../firebase/config";
+
+class CreatePost extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+     
+    }}}
